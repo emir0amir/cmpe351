6b04234074601700b4daedc2cf738a41b0104a7f
emir0amir <emir000amir@gmail.com>
2025-12-30 22:31:49 +0000

    new commit

---
+++ cmpe351.cpp
@@ -0,0 +1,17 @@
+//SIMULATING THE SERVICE OF JOBS IN CPU SCHEDULING  
+#include<iostream>
+#include<string.h>
+#include <string>
+#include <fstream>
+#include <cctype> 
+#include <cstdlib>  
+#include <sstream>  
+
+using namespace std;
+
+int globalstop = 1;
+int prem= 0;
+int fcfs = 0;
+int sjf = 0;
+int priority = 0;
+int scheduling = 0;
